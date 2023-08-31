87d43dc113cac3e10d72717fb404063ecb446971
Mariana <m.castaneda@ccbantigua.edu.gt>
2023-08-31 10:58:35 -0600

    Add info section

--- src/app/page.js
+++ src/app/page.js
@@ -19,6 +19,35 @@ export default function Home() {
           </h1>
         </div>
       </header>
+      <section className={styles.infoSection}>
+        <img
+        src="/la joven de la perla2.jpg"
+        alt="Imagen"
+        className={styles.image}
+        >
+        </img>
+        <div className={styles.infoContainer}>
+          <span className={styles.title + " " + styles.yellowText}>Josh</span>
+          <br/>
+          <span className={styles.title}>Carlson</span>
+          <div className={styles.list}>
+            <ul>
+              <li>
+                <span className={styles.grayText}>Age: </span>27
+              </li>
+              <li>
+                <span className={styles.grayText}>Nationality: </span>German
+              </li>
+              <li>
+                <span className={styles.grayText}>Skill set: </span>proyect Managment and financial performance
+              </li>
+              <li>
+                <span className={styles.grayText}>Languages: </span>English, German
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
     </main>
   )
 }
